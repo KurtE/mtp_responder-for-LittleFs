ae1088d8e6538622a3b500aa67ce53b271890cd4
Kurt Eckhardt <kurte@rockisland.com>
2020-11-12 06:32:06 -0800

    Update mtp-test.ino

--- MTP_t4_littlefs/examples/mtp-test/mtp-test.ino
+++ MTP_t4_littlefs/examples/mtp-test/mtp-test.ino
@@ -1,50 +1,69 @@
 #include "Arduino.h"
-//#define use_spi_disk
+#define use_spi_disk
 //#define use_qspi_disk
-#define use_ram_disk
+//#define use_ram_disk
+
+#include "MTP.h"
+#include "usb1_mtp.h"
+
+#if defined(use_spi_disk)
+MTPStorage_SPI storage;
+#elif defined(use_qspi_disk)
+MTPStorage_QSPI storage;
+#elif defined(use_ram_disk)
 DMAMEM char my_buffer[400000];
-
-  #include "MTP.h"
-  #include "usb1_mtp.h"
-
-  //MTPStorage_SPI storage;
-  //MTPStorage_QSPI storage;
-  MTPStorage_RAM storage;
-  MTPD       mtpd(&storage);
+MTPStorage_RAM storage;
+#else
+#error "Need to define one of the storage classes to use"
+#endif
+MTPD       mtpd(&storage);
 
 
 void logg(uint32_t del, const char *txt)
 { static uint32_t to;
-  if(millis()-to > del)
+  if (millis() - to > del)
   {
-    //Serial.println(txt); 
+    //Serial.println(txt);
 #if USE_SDIO==1
-    digitalWriteFast(2,!digitalReadFast(2));
+    digitalWriteFast(2, !digitalReadFast(2));
 #endif
-    to=millis();
+    to = millis();
   }
 }
 
 void setup()
-{ 
-  while(!Serial && millis()<2000); 
+{
+  while (!Serial && millis() < 2000);
   usb_mtp_configure();
-    //if(!Storage_init_qspi()) {Serial.println("No storage"); while(1);};
-    //if(!Storage_init_spi(6, SPI)) {Serial.println("No storage"); while(1);};
-    if(!Storage_init_ram(my_buffer, sizeof(my_buffer))) {Serial.println("No storage"); while(1);};
+#if defined(use_spi_disk)
+  if (!Storage_init_spi(6, SPI)) {
+    Serial.println("No storage");
+    while (1);
+  };
+#elif defined(use_qspi_disk)
+  if (!Storage_init_qspi()) {
+    Serial.println("No storage");
+    while (1);
+  };
+#elif defined(use_ram_disk)
+  if (!Storage_init_ram(my_buffer, sizeof(my_buffer))) {
+    Serial.println("No storage");
+    while (1);
+  };
+#endif
 
   Serial.println("MTP test");
 
 #if USE_SDIO==1
-  pinMode(2,OUTPUT);
+  pinMode(2, OUTPUT);
 #endif
 
 }
 
 void loop()
-{ 
+{
   mtpd.loop();
 
-  logg(1000,"loop");
+  logg(1000, "loop");
   //asm("wfi"); // may wait forever on T4.x
 }
